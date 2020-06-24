44cd446dcd69076c1d31aadd2bd1d44ca632062f
哆啦A梦 <1841084366@qq.com>
2020-06-24 14:55:40 +0800

    axios invoke method params modify

--- template/cover/index.js
+++ template/cover/index.js
@@ -2,11 +2,7 @@ import axios from './instance'
 
 <% _.forEach(data.mocks, function(mock){ %>/** {{mock.description}} */
 function {{$$.toCamelMethod($$.convertMethod(mock))}} (opts) {
-  return axios.{{mock.method}}({
-    method: '{{mock.method}}',
-    url: '{{mock.url}}',
-    opts: opts
-  })
+  return axios.{{mock.method}}('{{mock.url}}', opts)
 }
 
 <%}) %>
